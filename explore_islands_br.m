86c277174aa240e6c99f9ec99d802f7bc2e15302
binghui89 <bli6@ncsu.edu>
2019-07-03 10:23:10 -0500

    Use iteration-based approach to explore static islands and lolp.

--- explore_islands_br.m
+++ explore_islands_br.m
@@ -1,196 +1,97 @@
-%% Random attack
-
 clear;
 tic;
-load('delbr.mat')
 tx2kb = loadcase('./ACTIVSg2000/case_ACTIVSg2000.m');
-[unique_branch, i_d2u, i_u2d] = unique(tx2kb.branch(:, 1:2), 'rows');
+tx2kb.gen(strcmp(tx2kb.gentype, 'W2'), 9:10) = 0; % Remove wind generators
+tx2kb.gen(strcmp(tx2kb.gentype, 'PV'), 9:10) = 0; % Remove wind generators
+
 
 load_matrix = csvread('./ACTIVSg2000/Jubeyer/Texas_2k_load.csv', 1, 1);
 load_matrix(:, end) = []; % This column is total load
 i_loadbus = tx2kb.bus(:, 3)>0;
-nd = size(load_matrix,1);
 
-
+nd = size(load_matrix,1)/24; % We only look at peak hours per day
 nI = 20;
 nJ = 50;
-nislands_random = nan(nI, nJ);
-lolp_random     = nan(nI, nJ);
 
-for i = 1: nI
-    for j = 1: nJ
-        if i <= size(cell_delbr, 1)
+criteria = {'random', 'sumdegree', 'MVA'};
+ncr = numel(criteria);
+
+for c = 1: ncr
+    
+    cr = criteria{c};
+    
+    switch cr
+        case 'sumdegree'
+            load('delbr.mat', 'cell_delbr_sumdegree');
+            cell_delbr = cell_delbr_sumdegree;
+        case 'MVA'
+            load('delbr.mat', 'cell_delbr_MVA');
+            cell_delbr = cell_delbr_MVA;
+        case 'random'
+            load('delbr.mat', 'cell_delbr');
+    end
+
+    nislands = nan(nI, nJ);
+    lolp     = nan(nI, nd, nJ);
+
+    for i = 1: nI
+        for j = 1: nJ
             branchdel = cell_delbr{i, j};
-        else % Generate on fly
-            ndel = 10*i;
-            iunique_removed = randsample(1:size(unique_branch, 1), ndel);
-            branchdel = unique_branch(iunique_removed, :);
+
+            if isempty(branchdel)
+                continue;
+            end
+
+            parfor d = 1: nd
+
+                load_day = load_matrix(24*(d-1)+1: 24*d, :);
+                [~, imax] = max(sum(load_day, 2));
+
+                test = tx2kb;
+                test.bus(i_loadbus, 3) = load_day(imax, :)';
+                test = remove_br1(test, branchdel);
+    %             loss_of_load(d) = lolp_static(test);
+                lolp(i, d, j) = lolp_static(test);
+
+            end
+            test = remove_br1(tx2kb, branchdel);
+            cell_islands = extract_islands(test);
+            nislands(i, j) = numel(cell_islands);
+    %         lolp_random(i, j) = sum(loss_of_load)/nd;
+
         end
-        
-        loss_of_load = nan(size(load_matrix, 1)/24, 1);
-
-        parfor d = 1: size(load_matrix, 1)/24
-            
-            load_day = load_matrix(24*(d-1)+1: 24*d, :);
-            [~, imax] = max(sum(load_day, 2));
-            
-            test = tx2kb;
-            test.bus(i_loadbus, 3) = load_day(imax, :)';
-            test = remove_br1(test, branchdel);
-            loss_of_load(d) = lolp_static(test);
-
-        end
-        test = remove_br1(tx2kb, branchdel);
-        cell_islands = extract_islands(test);
-        nislands_random(i, j) = numel(cell_islands);
-        lolp_random(i, j) = sum(loss_of_load)/nd;
-
+        toc;
     end
-    toc;
-end
-
-if isfile('explore_islands_br.mat')
-    save('explore_islands_br.mat', 'nislands_random', 'lolp_random', '-append');
-else
-    save('explore_islands_br.mat', 'nislands_random', 'lolp_random');
-end
-
-%% MVA ratings
-clear;
-tx2kb = loadcase('./ACTIVSg2000/case_ACTIVSg2000.m');
-[edge_unique, i_d2u, i_u2d] = unique(tx2kb.branch(:, 1:2), 'rows');
-
-load_matrix = csvread('./ACTIVSg2000/Jubeyer/Texas_2k_load.csv', 1, 1);
-load_matrix(:, end) = []; % This column is total load
-i_loadbus = tx2kb.bus(:, 3)>0;
-nd = size(load_matrix,1);
-
-mva_rating = nan(size(edge_unique, 1), 1);
-for i = 1: size(edge_unique, 1)
-    bus_from = edge_unique(i, 1);
-    bus_to   = edge_unique(i, 2);
-    i_rows = (tx2kb.branch(:, 1) == bus_from) & (tx2kb.branch(:, 2) == bus_to);
-    mva_rating(i) = sum(tx2kb.branch(i_rows, 6)); % Column 6 is MVA ratings
-end
-edge_unique = [edge_unique, mva_rating];
-edge_unique_sorted = sortrows(edge_unique, 3, 'descend'); 
-edge_descend = edge_unique_sorted(:, 1:2); % This is the order of removed branches
-
-nedge_remove = 10: 10: 200;
-nI = length(nedge_remove);
-nislands_mva_rating = nan(nI, 1);
-lolp_mva_rating = nan(nI, 1);
-
-for i = 1: nI
-    ne_del = nedge_remove(i);
-    branchdel = edge_descend(1: ne_del, :);
     
-    loss_of_load = nan(size(load_matrix, 1)/24, 1);
-    
-    parfor d = 1: size(load_matrix, 1)/24
+    switch cr
+        case 'sumdegree'
+            nislands_sumdegree = nislands;
+            lolp_sumdegree = lolp;
+
+            if isfile('explore_islands_br.mat')
+                save('explore_islands_br.mat', 'nislands_sumdegree', 'lolp_sumdegree', '-append');
+            else
+                save('explore_islands_br.mat', 'nislands_sumdegree', 'lolp_sumdegree');
+            end
+
+        case 'MVA'
+            nislands_MVA = nislands;
+            lolp_MVA = lolp;
             
-        load_day = load_matrix(24*(d-1)+1: 24*d, :);
-        [~, imax] = max(sum(load_day, 2));
-
-        test = tx2kb;
-        test.bus(i_loadbus, 3) = load_day(imax, :)';
-        test = remove_br1(test, branchdel);
-        loss_of_load(d) = lolp_static(test);
-
-    end
-    lolp_mva_rating(i) = sum(loss_of_load)/nd;
-
-
-    test = remove_br1(tx2kb, branchdel);
-    ndel = size(branchdel, 1);
-
-    % Display how many isolated islands are left
-    cell_islands = extract_islands(test);
-    nislands_mva_rating(i) = numel(cell_islands);
-end
-
-if isfile('explore_islands_br.mat')
-    save('explore_islands_br.mat', 'nislands_mva_rating', 'lolp_mva_rating', '-append');
-else
-    save('explore_islands_br.mat', 'nislands_mva_rating', 'lolp_mva_rating');
-end
-
-% Sum of degrees of from-bus and to-bus
-tic;
-clear;
-tx2kb = loadcase('./ACTIVSg2000/case_ACTIVSg2000.m');
-tx2kb_br_ordered = tx2kb;
-
-load_matrix = csvread('./ACTIVSg2000/Jubeyer/Texas_2k_load.csv', 1, 1);
-load_matrix(:, end) = []; % This column is total load
-i_loadbus = tx2kb.bus(:, 3)>0;
-nd = size(load_matrix,1);
-
-for i = 1: size(tx2kb_br_ordered.branch, 1)
-    bus_from = tx2kb_br_ordered.branch(i, 1);
-    bus_to   = tx2kb_br_ordered.branch(i, 2);
-    if bus_from > bus_to
-        tx2kb_br_ordered.branch(i, 1) = bus_to;
-        tx2kb_br_ordered.branch(i, 2) = bus_from;
-    end
-end
-[edge_unique, i_d2u, i_u2d] = unique(tx2kb_br_ordered.branch(:, 1:2), 'rows');
-nedge = size(edge_unique, 1);
-s = cell(nedge, 1);
-t = cell(nedge, 1);
-for i = 1: nedge
-    s{i} = int2str(edge_unique(i, 1));
-    t{i} = int2str(edge_unique(i, 2));
-end
-
-G = graph(s, t);
-G.Nodes.Degree = degree(G);
-edge_degree_sum = zeros(nedge, 1);
-for i = 1: nedge
-    bus_from = int2str(edge_unique(i, 1));
-    bus_to   = int2str(edge_unique(i, 2));
-    edge_degree_sum(i) = G.Nodes.Degree(strcmp(G.Nodes.Name, bus_from)) + G.Nodes.Degree(strcmp(G.Nodes.Name, bus_to));
-end
-edge_unique = [edge_unique, edge_degree_sum];
-edge_unique_sorted = sortrows(edge_unique, 3, 'descend'); % Sort by degree sum
-edge_descend = edge_unique_sorted(:, 1:2); % This is the order of removed branches
-
-nedge_remove = 10: 10: 200;
-nI = length(nedge_remove);
-nislands_sum_degree = nan(nI, 1);
-lolp_sum_degree = nan(nI, 1);
-
-for i = 1: nI
-    ne_del = nedge_remove(i);
-    branchdel = edge_descend(1: ne_del, :);
-    
-    loss_of_load = nan(size(load_matrix, 1)/24, 1);
-    
-    parfor d = 1: size(load_matrix, 1)/24
+            if isfile('explore_islands_br.mat')
+                save('explore_islands_br.mat', 'nislands_MVA', 'lolp_MVA', '-append');
+            else
+                save('explore_islands_br.mat', 'nislands_MVA', 'lolp_MVA');
+            end
+        case 'random'
+            nislands_random = nislands;
+            lolp_random = lolp;
             
-        load_day = load_matrix(24*(d-1)+1: 24*d, :);
-        [~, imax] = max(sum(load_day, 2));
-
-        test = tx2kb;
-        test.bus(i_loadbus, 3) = load_day(imax, :)';
-        test = remove_br1(test, branchdel);
-        loss_of_load(d) = lolp_static(test);
-
+            if isfile('explore_islands_br.mat')
+                save('explore_islands_br.mat', 'nislands_random', 'lolp_random', '-append');
+            else
+                save('explore_islands_br.mat', 'nislands_random', 'lolp_random');
+            end
     end
-    lolp_sum_degree(i) = sum(loss_of_load)/nd;
 
-
-    test = remove_br1(tx2kb, branchdel);
-    ndel = size(branchdel, 1);
-
-    % Display how many isolated islands are left
-    cell_islands = extract_islands(test);
-    nislands_sum_degree(i) = numel(cell_islands);
 end
-
-if isfile('explore_islands_br.mat')
-    save('explore_islands_br.mat', 'nislands_sum_degree', 'lolp_sum_degree', '-append');
-else
-    save('explore_islands_br.mat', 'nislands_sum_degree', 'lolp_sum_degree');
-end
-toc;
--- explore_islands_nd.m
+++ explore_islands_nd.m
@@ -1,145 +1,122 @@
-%% Random attack
-
+%% New iteration-based approach
 clear;
 tic;
-load('delnd.mat');
 tx2kb = loadcase('./ACTIVSg2000/case_ACTIVSg2000.m');
+tx2kb.gen(strcmp(tx2kb.gentype, 'W2'), 9:10) = 0; % Remove wind generators
+tx2kb.gen(strcmp(tx2kb.gentype, 'PV'), 9:10) = 0; % Remove wind generators
+
 
 load_matrix = csvread('./ACTIVSg2000/Jubeyer/Texas_2k_load.csv', 1, 1);
 load_matrix(:, end) = []; % This column is total load
 i_loadbus = tx2kb.bus(:, 3)>0;
 
+nd = size(load_matrix,1)/24; % We only look at peak hours per day
+nI = 20;
+nJ = 50;
 
-nislands_random = nan(nI, nJ);
-lolp_random     = nan(nI, nJ);
-nnode_remove    = nan(nI, 1);
+criteria = {'random', 'degree', 'sumMVA', 'sumPMAX', 'baseKV'};
+ncr = numel(criteria);
 
-for i = 1: nI
-    for j = 1: nJ % Just repeat 10 times per attack
-        
-        loss_of_load = nan(size(load_matrix, 1)/24, 1);
-        
-        nodedel = cell_delnd{i, j};
+for c = 1: ncr
+    
+    cr = criteria{c};
+    
+    switch cr
+        case 'degree'
+            load('delnd.mat', 'cell_delnd_degree');
+            cell_delnd = cell_delnd_degree;
+        case 'sumMVA'
+            load('delnd.mat', 'cell_delnd_sumMVA');
+            cell_delnd = cell_delnd_sumMVA;
+        case 'sumPMAX'
+            load('delnd.mat', 'cell_delnd_sumPMAX');
+            cell_delnd = cell_delnd_sumPMAX;
+        case 'baseKV'
+            load('delnd.mat', 'cell_delnd_baseKV');
+            cell_delnd = cell_delnd_baseKV;
+        case 'random'
+            load('delnd.mat', 'cell_delnd');
+    end
 
-        parfor d = 1: size(load_matrix, 1)/24
-            
-            load_day = load_matrix(24*(d-1)+1: 24*d, :);
-            [~, imax] = max(sum(load_day, 2));
-            
-            test = tx2kb;
-            test.bus(i_loadbus, 3) = load_day(imax, :)';
-            test = remove_nd(test, nodedel);
-            loss_of_load(d) = lolp_static(test);
+    nislands = nan(nI, nJ);
+    lolp     = nan(nI, nd, nJ);
+
+    for i = 1: nI
+        for j = 1: nJ
+            nodedel = cell_delnd{i, j};
+
+            if isempty(nodedel)
+                continue;
+            end
+
+            parfor d = 1: nd
+
+                load_day = load_matrix(24*(d-1)+1: 24*d, :);
+                [~, imax] = max(sum(load_day, 2));
+
+                test = tx2kb;
+                test.bus(i_loadbus, 3) = load_day(imax, :)';
+                test = remove_nd(test, nodedel);
+    %             loss_of_load(d) = lolp_static(test);
+                lolp(i, d, j) = lolp_static(test);
+
+            end
+            test = remove_nd(tx2kb, nodedel);
+            cell_islands = extract_islands(test);
+            nislands(i, j) = numel(cell_islands);
+    %         lolp_random(i, j) = sum(loss_of_load)/nd;
 
         end
-        
-        test = remove_nd(tx2kb, nodedel);
-        cell_islands = extract_islands(test);
-        nislands_random(i, j) = numel(cell_islands);
-        lolp_random(i, j) = sum(loss_of_load)/numel(loss_of_load);
-
+        toc;
     end
-    nnode_remove(i) = size(nodedel, 1);
-    toc;
+    
+    switch cr
+        case 'degree'
+            nislands_degree = nislands;
+            lolp_degree = lolp;
+
+            if isfile('explore_islands_nd.mat')
+                save('explore_islands_nd.mat', 'nislands_degree', 'lolp_degree', '-append');
+            else
+                save('explore_islands_nd.mat', 'nislands_degree', 'lolp_degree');
+            end
+
+        case 'sumMVA'
+            nislands_sumMVA = nislands;
+            lolp_sumMVA = lolp;
+            
+            if isfile('explore_islands_nd.mat')
+                save('explore_islands_nd.mat', 'nislands_sumMVA', 'lolp_sumMVA', '-append');
+            else
+                save('explore_islands_nd.mat', 'nislands_sumMVA', 'lolp_sumMVA');
+            end
+        case 'sumPMAX'
+            nislands_sumPMAX = nislands;
+            lolp_sumPMAX = lolp;
+            
+            if isfile('explore_islands_nd.mat')
+                save('explore_islands_nd.mat', 'nislands_sumPMAX', 'lolp_sumPMAX', '-append');
+            else
+                save('explore_islands_nd.mat', 'nislands_sumPMAX', 'lolp_sumPMAX');
+            end
+        case 'baseKV'
+            nislands_baseKV = nislands;
+            lolp_baseKV = lolp;
+            
+            if isfile('explore_islands_nd.mat')
+                save('explore_islands_nd.mat', 'nislands_baseKV', 'lolp_baseKV', '-append');
+            else
+                save('explore_islands_nd.mat', 'nislands_baseKV', 'lolp_baseKV');
+            end
+        case 'random'
+            nislands_random = nislands;
+            lolp_random = lolp;
+            
+            if isfile('explore_islands_nd.mat')
+                save('explore_islands_nd.mat', 'nislands_random', 'lolp_random', '-append');
+            else
+                save('explore_islands_nd.mat', 'nislands_random', 'lolp_random');
+            end
+    end
+
 end
-
-if isfile('explore_islands_nd.mat')
-    save('explore_islands_nd.mat', 'nislands_random', 'lolp_random', 'nnode_remove', '-append');
-else
-    save('explore_islands_nd.mat', 'nislands_random', 'lolp_random', 'nnode_remove');
-end
-
-%% By criteria
-
-% clear;
-% tx2kb = loadcase('./ACTIVSg2000/case_ACTIVSg2000.m');
-% [edge_unique, i_d2u, i_u2d] = unique(tx2kb.branch(:, 1:2), 'rows');
-% 
-% load_matrix = csvread('./ACTIVSg2000/Jubeyer/Texas_2k_load.csv', 1, 1);
-% load_matrix(:, end) = []; % This column is total load
-% i_loadbus = tx2kb.bus(:, 3)>0;
-% nd = size(load_matrix,1);
-% 
-% remove_by = {'degree'; 'sumMVA' ; 'sumPMAX'; 'baseKV'};
-% 
-% 
-% for k = 1: size(remove_by, 1)
-%     remove_by_this = remove_by{k};
-%     node_descend = node_order_by(remove_by_this);
-% 
-%     nnode_remove = 10: 10: 200;
-%     nI = length(nnode_remove);
-%     nislands = nan(nI, 1);
-%     lolp = nan(nI, 1);
-% 
-%     for i = 1: nI
-%         n_del = nnode_remove(i);
-%         nodedel = node_descend(1: n_del, :);
-%         loss_of_load = nan(size(load_matrix, 1)/24, 1);
-% 
-%         parfor d = 1: size(load_matrix, 1)/24
-% 
-%             load_day = load_matrix(24*(d-1)+1: 24*d, :);
-%             [~, imax] = max(sum(load_day, 2));
-%             test = tx2kb;
-%             test.bus(i_loadbus, 3) = load_day(imax, :)';
-% 
-%             test = remove_nd(test, nodedel);
-%             loss_of_load(d) = lolp_static(test);
-% 
-%         end
-%         lolp(i) = sum(loss_of_load)/numel(loss_of_load);
-% 
-% 
-%         test = remove_nd(tx2kb, nodedel);
-%         ndel = size(nodedel, 1);
-% 
-%         % Display how many isolated islands are left
-%         cell_islands = extract_islands(test);
-%         nislands(i) = numel(cell_islands);
-%     end
-%     
-%     switch remove_by_this
-%         case 'degree'
-%             nislands_degree = nislands;
-%             lolp_degree = lolp;
-% 
-%             if isfile('explore_islands_nd.mat')
-%                 save('explore_islands_nd.mat', 'nislands_degree', 'lolp_degree', 'nnode_remove', '-append');
-%             else
-%                 save('explore_islands_nd.mat', 'nislands_degree', 'lolp_degree', 'nnode_remove');
-%             end
-%     
-%         case 'sumMVA'
-%             nislands_sumMVA = nislands;
-%             lolp_sumMVA = lolp;
-% 
-%             if isfile('explore_islands_nd.mat')
-%                 save('explore_islands_nd.mat', 'nislands_sumMVA', 'lolp_sumMVA', 'nnode_remove', '-append');
-%             else
-%                 save('explore_islands_nd.mat', 'nislands_sumMVA', 'lolp_sumMVA', 'nnode_remove');
-%             end
-%             
-%         case 'sumPMAX'
-%             nislands_sumPMAX = nislands;
-%             lolp_sumPMAX = lolp;
-%             
-%             if isfile('explore_islands_nd.mat')
-%                 save('explore_islands_nd.mat', 'nislands_sumPMAX', 'lolp_sumPMAX', 'nnode_remove', '-append');
-%             else
-%                 save('explore_islands_nd.mat', 'nislands_sumPMAX', 'lolp_sumPMAX', 'nnode_remove');
-%             end
-% 
-%         case 'baseKV'
-%             nislands_baseKV = nislands;
-%             lolp_baseKV = lolp;
-%             
-%         if isfile('explore_islands_nd.mat')
-%             save('explore_islands_nd.mat', 'nislands_baseKV', 'lolp_baseKV', 'nnode_remove', '-append');
-%         else
-%             save('explore_islands_nd.mat', 'nislands_baseKV', 'lolp_baseKV', 'nnode_remove');
-%         end
-% 
-%     end
-% 
-% end
